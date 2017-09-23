1ea7da516f3753444f20d5e0374a273bd9ad5ad7
KaitoHH <hh.kaito@gmail.com>
2017-09-23 22:31:12 +0800

    complete basic feature

--- Solver.h
+++ Solver.h
@@ -1,52 +1,86 @@
-#include "UnarySlot.h"
 #include "rapidjson/document.h"
-#include "rapidjson/writer.h"
-#include "rapidjson/stringbuffer.h"
-#include <vector>
+#include "UnarySlot.h"
 #include <queue>
+#include <set>
 #include <iostream>
 #include <fstream>
 #include <memory>
+
 class Solver;
 class SolverConfig;
+
 typedef std::shared_ptr<SolverConfig> config_ptr;
 using namespace rapidjson;
 
-class SolverConfig
-{
+class SolverConfig {
 private:
-    SolverConfig() {};
-    int begin;
-    int target;
-    std::vector<unary>runnerList;
-    friend Solver;
+	SolverConfig() {};
+	int begin;
+	int target;
+	std::vector<_unary::Unary *> runnerList;
+	friend Solver;
 
 public:
-    static config_ptr readConfigFromFile(const char *fileName) {
-        std::ifstream ifs(fileName);
-        std::string content((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
-        Document document;
-        document.Parse(content.c_str());
+	static config_ptr readConfigFromFile(const char *fileName) {
+		std::ifstream ifs(fileName);
+		std::string content((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+		Document document;
+		document.Parse(content.c_str());
 
-        config_ptr config(new SolverConfig());
-        config->begin = document["begin"].GetInt();
-        config->target = document["target"].GetInt();
-        
-        
-        
-        return config;
-    }
+		config_ptr config(new SolverConfig());
+		config->begin = document["begin"].GetInt();
+		config->target = document["target"].GetInt();
+
+		for (auto &x : document["methods"].GetArray()) {
+			std::vector<int> argList;
+			if (x.FindMember("param") != x.MemberEnd())
+				for (auto &y:x["param"].GetArray())
+					argList.push_back(y.GetInt());
+			config->runnerList.push_back(_unary::UnaryFactory::createUnaryFunction(x["type"].GetString(), argList));
+		}
+
+		return config;
+	}
 };
 
-class Solver
-{
+class Solver {
 private:
-    config_ptr config;
+	config_ptr config;
 public:
-    Solver(config_ptr config) {
-        this->config = config;
-    }
-    solve() {
+	Solver(config_ptr config) {
+		this->config = config;
+	}
 
-    }
+	void solve() {
+		std::queue<int> q;
+		std::set<int> vis;
+		std::map<int, int> fa;
+		q.push(config->begin);
+		vis.insert(config->begin);
+		while (!q.empty()) {
+			int cur = q.front();
+			if (cur == config->target) {
+				break;
+			}
+			q.pop();
+			for (auto &f : config->runnerList) {
+				int new_cur = f->calc(cur);
+				if (vis.find(new_cur) == vis.end()) {
+					fa[new_cur] = cur;
+					vis.insert(new_cur);
+					q.push(new_cur);
+				}
+			}
+		}
+		std::vector<int> ans;
+		int cur = config->target;
+		while (cur != config->begin) {
+			ans.push_back(cur);
+			cur = fa[cur];
+		}
+		ans.push_back(cur);
+		for (auto it = ans.rbegin(); it != ans.rend(); it++) {
+			std::cout << *it << std::endl;
+		}
+	}
 };
--- UnarySlot.h
+++ UnarySlot.h
@@ -1,5 +1,175 @@
 #pragma once
+
 #include <map>
-typedef int (*unary) (int);
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+std::map<int, int> mp;
+namespace _unary {
+	class UnaryFactory;
+
+	class Unary;
+	class SingleArgUnary;
+	class DoubleArgUnary;
+	class AddUnary;
+	class MinusUnary;
+	class MultiUnary;
+	class DivUnary;
+	class DelUnary;
+	class InputUnary;
+	class ConvertUnary;
+
+	typedef Unary *(*UnaryFactoryFunc)(const std::vector<int> &);
+
+	typedef std::map<std::string, UnaryFactoryFunc> UnaryMap;
+
+	extern UnaryMap funcMap;
+
+}
+
+class _unary::Unary {
+public:
+	virtual int calc(int) = 0;
+};
+
+class _unary::SingleArgUnary : public _unary::Unary {
+protected:
+	int arg;
+public:
+	explicit SingleArgUnary(int arg) : arg(arg) {}
+};
+
+class _unary::DoubleArgUnary : public _unary::Unary {
+protected:
+	int arg;
+	int arg2;
+public:
+	explicit DoubleArgUnary(int arg, int arg2) : arg(arg), arg2(arg2) {}
+};
+
+class _unary::AddUnary : public _unary::SingleArgUnary {
+public:
+	explicit AddUnary(int arg) : SingleArgUnary(arg) {}
+
+	int calc(int num) override {
+		return num + arg;
+	}
+
+};
+
+class _unary::MinusUnary : public _unary::SingleArgUnary {
+public:
+	explicit MinusUnary(int arg) : SingleArgUnary(arg) {}
+
+	int calc(int num) override {
+		return num - arg;
+	}
+
+};
+
+class _unary::MultiUnary : public _unary::SingleArgUnary {
+public:
+	explicit MultiUnary(int arg) : SingleArgUnary(arg) {}
 
 
+	int calc(int num) override {
+		return num * arg;
+	}
+
+};
+
+class _unary::DivUnary : public _unary::SingleArgUnary {
+public:
+	explicit DivUnary(int arg) : SingleArgUnary(arg) {}
+
+	int calc(int num) override {
+		return num / arg;
+	}
+
+};
+
+class _unary::DelUnary : public _unary::Unary {
+public:
+	int calc(int num) override {
+		return num / 10;
+	}
+
+};
+
+class _unary::InputUnary : public _unary::SingleArgUnary {
+public:
+	explicit InputUnary(int arg) : SingleArgUnary(arg) {}
+
+	int calc(int num) override {
+		int temp = arg;
+		do {
+			num *= 10;
+			temp /= 10;
+		} while (temp);
+		return num + (num < 0 ? -arg : arg);
+	}
+
+};
+
+class _unary::ConvertUnary : public _unary::DoubleArgUnary {
+public:
+	ConvertUnary(int arg, int arg2) : DoubleArgUnary(arg, arg2) {}
+
+	int calc(int num) override {
+		std::string temp = std::to_string(num);
+		std::replace(temp.begin(), temp.end(), arg + '0', arg2 + '0');
+		return std::stoi(temp);
+	}
+
+};
+
+class _unary::UnaryFactory {
+public:
+	static Unary *createUnaryFunction(const char *method, const std::vector<int> &array) {
+		auto f = _unary::funcMap[method];
+		return f(array);
+	}
+
+	static Unary *createAddFunction(const std::vector<int> &array) {
+		return new AddUnary(array[0]);
+	}
+
+	static Unary *createMinusFunction(const std::vector<int> &array) {
+		return new MinusUnary(array[0]);
+	}
+
+	static Unary *createMultiFunction(const std::vector<int> &array) {
+		return new MultiUnary(array[0]);
+	}
+
+	static Unary *createDivFunction(const std::vector<int> &array) {
+		return new DivUnary(array[0]);
+	}
+
+	static Unary *createDelFunction(const std::vector<int> &array) {
+		return new DelUnary();
+	}
+
+	static Unary *createInputFunction(const std::vector<int> &array) {
+		return new InputUnary(array[0]);
+	}
+
+	static Unary *createConvertFunction(const std::vector<int> &array) {
+		return new ConvertUnary(array[0], array[1]);
+	}
+};
+
+
+_unary::UnaryMap _unary::funcMap = ([]() -> _unary::UnaryMap & {
+	using namespace _unary;
+	static UnaryMap funcMap;
+	funcMap["add"] = UnaryFactory::createAddFunction;
+	funcMap["minus"] = UnaryFactory::createMinusFunction;
+	funcMap["multi"] = UnaryFactory::createMultiFunction;
+	funcMap["div"] = UnaryFactory::createDivFunction;
+	funcMap["input"] = UnaryFactory::createInputFunction;
+	funcMap["delete"] = UnaryFactory::createDelFunction;
+	funcMap["convert"] = UnaryFactory::createConvertFunction;
+	return funcMap;
+})();
